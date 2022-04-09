f50e8e977ea81d97968120c9ac4c8f36b6d87532
Nicole Valentini <ncvalentini@gmail.com>
2022-04-09 17:39:20 -0600

    Fix port connection

--- server.js
+++ server.js
@@ -73,4 +73,4 @@ const validateCourse = (course) => {
 }
 
 const port = process.env.PORT || 3000;
-app.listen(3000, () => console.log(`Listening on port ${`3000`}...`))
+app.listen(PORT, () => console.log(`Listening on port ${`3000`}...`))
